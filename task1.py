eb7d143a042b365712cc7ede4a4f069852c851af
harutoyyy <haluomu0530@gmail.com>
2024-05-01 13:34:04 -0700

    COMP

--- task1.py
+++ task1.py
@@ -1,6 +1,25 @@
+#Task 1:
+#Create a program with 3 function definitions:
+#function A prints the message "Hello"
+#function B prints the message "How are you"
+#function C prints the message "Hi"
+
+#Ask the user to enter a letter from A to C
+#Execute the function of the letter they use.
+
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you")
 
 def C():
+    print("Hi")
+
+x = str(input("Enter a letter from A to C"))
+if x == "A":
+    A()
+elif x == "B":
+    B()
+elif x == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,44 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+
+def title():
+    print("Guessing number game")
+
+def game():
+    import random
+    import math
+
+    lower = int(input("Enter Lower bound:"))
+    upper = int(input("Enter Upper bound:"))
+
+
+    x = random.randint(lower, upper)
+    print("\n\tYou've only ", 
+        round(math.log(upper - lower + 1, 2)),
+        " chances to guess the integer!\n")
+
+    count = 0
+
+    while count < math.log(upper - lower + 1, 2):
+        count += 1
+
+        guess = int(input("Guess a number:- "))
+    
+
+        if x == guess:
+            print("Congratulations you did it in ",
+                count, " try")
+            break
+        elif x > guess:
+            print("You guessed too small!")
+        elif x < guess:
+            print("You Guessed too high!")
+
+    if count >= math.log(upper - lower + 1, 2):
+        print("\nThe number is %d" % x)
+        print("\tBetter Luck Next time!")
+    
+title()
+game()
